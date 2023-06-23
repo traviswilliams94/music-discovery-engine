2c0c10313302ed5896d64f4e5750c1ced0d8230a
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-23 14:41:03 -0600

    spotify api authorization framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect}  from 'react';
+
+const ApiBaseUrl = "https://api.spotify.com/v1";
+
 
 function App() {
+  const [apiKey, setApiKey] = useState("");
+
+  let bearerToken = `Bearer ${apiKey}`;
+
+  //post request to get a authorization key valid for 1 hour
+  useEffect(() => {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/x-www-form-urlencoded"
+      },
+      body: "grant_type=client_credentials&client_id=2c092ae6773d4bbfb315299881de041a&client_secret=78f58078f91d43bf906fc8fc1e915c76"
+    })
+    .then(res => res.json())
+    .then(data => {
+      const token = data.access_token;
+      setApiKey(token);
+    })
+  }, [])
+
+  //template function for how to use the authentication
+  function testGetGenreSeeds() {
+    fetch(`${ApiBaseUrl}/recommendations/available-genre-seeds`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: bearerToken,
+      },
+    })
+    .then(res => res.json())
+    .then(console.log)
+  }
+
+  //testGetGenreSeeds();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1></h1>
+      </div>
     </div>
   );
 }
