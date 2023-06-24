222cf2c80ae59de7988718b5e6cf92539d1764d9
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-24 17:09:48 -0600

    random button hardcoded to classical music, track component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {useState, useEffect}  from 'react';
-
-const ApiBaseUrl = "https://api.spotify.com/v1";
+import Track from "./Track.js";
 
 function App() {
   const [apiKey, setApiKey] = useState("");
+  const [tracks, setTracks] = useState([]);
 
   let bearerToken = `Bearer ${apiKey}`;
 
@@ -21,12 +21,50 @@ function App() {
     .then(data => {
       const token = data.access_token;
       setApiKey(token);
+      //console.log(token);
     })
   } , [])
 
+  //test genre
+  const genres = "classical";
+
+  function getRandomTracks() {
+    const request = generateRequestFromSeeds(genres, "", "");
+    fetch(request, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: bearerToken,
+      },
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.tracks);
+      setTracks(data.tracks);
+    });
+  }
+
+  //TODO fix this nightmare
+  function generateRequestFromSeeds(seedGenres , seedTracks, seedArtists) {
+    let stringRequest = "";
+    if (seedTracks !== undefined && seedArtists !== undefined) {
+      stringRequest = `https://api.spotify.com/v1/recommendations?seed_artists=${seedArtists}&seed_genres=${seedGenres}&seed_tracks=${seedTracks}`;
+    }
+    else if (seedTracks !== undefined) {
+      stringRequest = `https://api.spotify.com/v1/recommendations?seed_genres=${seedGenres}&seed_tracks=${seedTracks}`;
+    }
+    else if (seedArtists !== undefined) {
+      stringRequest = `https://api.spotify.com/v1/recommendations?seed_artists=${seedArtists}&seed_genres=${seedGenres}`;
+    }
+    else {
+      stringRequest = `https://api.spotify.com/v1/recommendations?seed_genres=${seedGenres}`;
+    }
+    console.log(stringRequest);
+    return stringRequest;
+  }
+
   //template function for how to use the authentication
   function template() {
-    fetch(`${ApiBaseUrl}/endpoint`, {//replace endpoint
+    fetch("https://api.spotify.com/v1/endpoint", {//replace endpoint
       headers: {
         "Content-Type": "application/json",
         Authorization: bearerToken,//will only work after useEffect async completes
@@ -36,8 +74,27 @@ function App() {
     .then()//do whatever
   }
 
+
+  //TODO turn into maybe an "add to playlist" button
+  function getID(track) {
+    console.log(track.id);
+  }
+  
+  //TODO the tracklist and other methods in here shouldn't be at the top level,
+  //I just didn't want to do all the routing work today
+  //let me know when you set up the routing(or when i do) and we can move things around
   return (
-    <div className="App">
+    <div className="App" id="Tracklist">
+      <button onClick={getRandomTracks}>Get Random Tracks</button>
+      {tracks ? 
+      tracks.map(track => {
+        return <Track 
+        key={track.name} 
+        track={track}
+        getID={getID}/>
+      })
+      : <h1>Loading...</h1>
+      }
     </div>
   );
 }
