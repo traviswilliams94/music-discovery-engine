3a477b4863c722d9ff87430dd65927b8bc1adec7
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-28 14:20:34 -0600

    commit sync

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -23,19 +23,21 @@ export default function Track({ track, inPlaylist }) {
 
   return (
     <div className="ui cards eight wide column">
-      <img src={album.images[1].url} alt={album.name}/>
-      <div className="ui header"><b>Title: </b>{name}</div>
-      <div className="ui meta">
-        {artists.map(artist => {
-          return <div key={artist.name}><b>Artist: </b>{artist.name}</div>;
-        })}
+      <div className="ui content">
+        <img src={album.images[1].url} alt={album.name} />
+        <div className="ui header">{name}</div>
+        <div className="ui artist">
+          {artists.map(artist => {
+            return <div key={artist.name}><b>Artist: </b>{artist.name}</div>;
+          })}
+        </div>
+        <div className="ui album">
+          <b>Album: </b> {album.name}
+        </div>
+        {inPlaylist ?
+          <button className='playlistbutton' onClick={removeFromPlaylist}>Remove from Saved Tracks</button>
+          : <button className='playlistbutton' onClick={addToPlaylist}>{saveState}</button>}
       </div>
-      <div className="ui meta">
-       <b>Album: </b> {album.name}
-      </div>
-      {inPlaylist ?
-      <button className='playlistbutton' onClick={removeFromPlaylist}>Remove from Saved Tracks</button>
-      : <button className='playlistbutton' onClick={addToPlaylist}>{saveState}</button>}
     </div>
   )
 }  
